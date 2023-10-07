f70c31c1b87ec50564e95f7f40d02bde26e46866
Neggin-h <negginhariri@gmail.com>
2023-10-07 19:04:57 +0330

    Removed current location button

--- src/index.js
+++ src/index.js
@@ -59,17 +59,6 @@ function handleSubmit(event) {
   searchCity(city);
 }
 
-function searchLocation(position) {
-  let apiKey = "1offb792af43d742310308a3bd54tb61";
-  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${position.coords.latitude}&lon=${position.coords.longitude}&appid=${apiKey}&units=metric`;
-
-  axios.get(apiUrl).then(displayWeatherCondition);
-}
-
-function getCurrentLocation(event) {
-  event.preventDefault();
-  navigator.geolocation.getCurrentPosition(searchLocation);
-}
 //Metrics
 function handleSubmit(event) {
   event.preventDefault();
